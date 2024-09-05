80bd4ee25f8098f82129069c5c301cf1ab032073
YPKIM1 <kimub1226@naver.com>
2024-09-05 13:34:40 +0900

    Login

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import Layout from "./components/Layout/Layout";
 import Main from "./components/Main/Main";
+import Login from "./components/Login/Login";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLogin(true);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path={"/"} element={<Layout />}>
-          <Route index element={<Main />} /> {/* 기본 경로 */}
-          <Route path="abc" element={<div>1234</div>} />
-          {/*<Route path="guestbook" element={<Guestbook />} /> 추가할 페이지들
+        {isLogin ? (
+          <Route path={"/"} element={<Layout />}>
+            <Route index element={<Main />} /> {/* 기본 경로 */}
+            <Route path="abc" element={<div>1234</div>} />
+            {/*<Route path="guestbook" element={<Guestbook />} /> 추가할 페이지들
           <Route path="*" element={<Error404 />} /> 404 페이지 */}
-        </Route>
+          </Route>
+        ) : (
+          <Route
+            path={"/"}
+            element={<Login onLoginSuccess={handleLoginSuccess} />}
+          />
+        )}
       </Routes>
     </Router>
   );
