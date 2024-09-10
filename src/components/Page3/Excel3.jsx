e8870b66dc9b859f12d7b9651c68102e5362b2d6
SongByoungje <songbyoungje@naver.com>
2024-09-10 16:42:10 +0900

    page3modify
    page3와 그 컴포넌트의 추가 및 수정

--- src/components/Page3/Excel3.jsx
+++ src/components/Page3/Excel3.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Excel3Table from "./Excel3Table";
-import Excel3Chart1 from "./Excel3Chart1";
+import Excel3Chart from "./Excel3Chart";
 
 function Excel3() {
   return (
@@ -9,20 +9,16 @@ function Excel3() {
       sx={{
         background: "white",
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
         padding: "10px",
-        width: "100%", // 너비가 부모 요소의 100%를 차지
-        maxWidth: "1900px", // 최대 너비 설정
-        height: "auto", // 높이 자동 조정
-        gap: "20px",
-        margin: "10px",
-        flexGrow: 0,
+        width: "1600px",
+        height: "500px",
+        gap: "10px", // gap을 줄여 여백 최소화
+        margin: "0px auto",
       }}
     >
-      <div>
-        <Excel3Table />
-        <Excel3Chart1 />
-      </div>
+      <Excel3Table />
+      <Excel3Chart />
     </Box>
   );
 }
--- src/components/Page3/Excel3Table.jsx
+++ src/components/Page3/Excel3Table.jsx
@@ -18,13 +18,41 @@ const rows = [
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 export default function Excel3Table() {
   // 컴포넌트 이름을 Excel3Table로 변경
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer
+      component={Paper}
+      sx={{ width: "100%", margin: "0 auto", padding: "0px" }}
+    >
+      {" "}
+      {/* 여백 줄이기 */}
+      <Table
+        sx={{ minWidth: 500, margin: "0px", padding: "0px" }}
+        aria-label="simple table"
+      >
         <TableHead>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
