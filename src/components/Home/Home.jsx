2a6b4dc92f375b825ba0f0826db5fc605e7ec569
SongByoungje <songbyoungje@naver.com>
2024-09-23 15:26:58 +0900

    mainhyperlink
    yeah

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -76,27 +76,59 @@ const Home = () => {
             </div>
           </div>
         </div>
+        <div className="weather-forecast">
+          {weeklyForecast.map((day, index) => (
+            <div key={index} className="weather-day">
+              <div className="date">{day.date}</div>
+              <div className="weather-image">
+                <i
+                  className={`ico ico_wt${day.weather[0].weatherCode}`} // 첫 번째 시간대의 weatherCode 사용
+                  data-tooltip={day.weather[0].weatherDescription} // 첫 번째 시간대의 weatherDescription 사용
+                />
+                <i
+                  className={`ico ico_wt${day.weather[1].weatherCode}`} // 첫 번째 시간대의 weatherCode 사용
+                  data-tooltip={day.weather[1].weatherDescription} // 첫 번째 시간대의 weatherDescription 사용
+                />
+              </div>
+              <div className="temp-range">
+                {`${day.weather[0].temperature.lowest}°C / `}
+                <span className="highest-temp">{`${day.weather[0].temperature.highest}°C`}</span>
+              </div>
+              <div className="rain-chance">{` ${day.weather[0].rainfall}% / ${day.weather[1].rainfall}%`}</div>
+
+              {/* <span className="blind">{day.weather[0].weatherDescription}</span> */}
+            </div>
+          ))}
+        </div>
       </div>
-      {/* <div className="container2">
+      <div className="container2">
+        <Box className="hyperlink">
+          <a
+            href="https://www.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google
+          </a>
+          <br />
+          <a
+            href="https://www.naver.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Naver
+          </a>
+          <br />
+          <a
+            href="http://www.samsungshi.com/Ko/Default.aspx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Samsung Heavy Industries
+          </a>
+        </Box>
+
         <ToDoList />
-      </div> */}
-      <div className="weather-forecast">
-        {weeklyForecast.map((day, index) => (
-          <div key={index} className="weather-day">
-            <div className="date">{day.date}</div>
-            <div className="temp-range">{`${day.weather[0].temperature.lowest}° / ${day.weather[0].temperature.highest}°`}</div>
-            <div className="rain-chance">{`Rain: ${day.weather[0].rainfall}% / ${day.weather[1].rainfall}%`}</div>
-            <i
-              className={`ico ico_wt${day.weather[0].weatherCode}`} // 첫 번째 시간대의 weatherCode 사용
-              data-tooltip={day.weather[0].weatherDescription} // 첫 번째 시간대의 weatherDescription 사용
-            />
-            <i
-              className={`ico ico_wt${day.weather[1].weatherCode}`} // 첫 번째 시간대의 weatherCode 사용
-              data-tooltip={day.weather[1].weatherDescription} // 첫 번째 시간대의 weatherDescription 사용
-            />
-            <span className="blind">{day.weather[0].weatherDescription}</span>
-          </div>
-        ))}
       </div>
     </div>
   );
